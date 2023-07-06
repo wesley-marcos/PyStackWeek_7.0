aa6752d529ada78ff8b41951056958a6c7829537
Wesley Marcos <wesley.marcos@gec.inatel.br>
2023-07-06 13:32:44 -0300

    extrato: Criação da tabela 'Valores' no banco de dados

--- extrato/models.py
+++ extrato/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from perfil.models import Categorias, Conta
 
-# Create your models here.
+class Valores(models.Model):
+    choice_tipo = (
+        ('E', 'Entrada'),
+        ('S', 'Saída')
+    )
+    
+    valor = models.FloatField()
+    categoria = models.ForeignKey(Categorias, on_delete=models.DO_NOTHING)
+    descricao = models.TextField()
+    data = models.DateField()
+    conta = models.ForeignKey(Conta, on_delete=models.DO_NOTHING)
+    tipo = models.CharField(max_length=1, choices=choice_tipo)
+
+    def __str__(self):
+        return self.descricao
