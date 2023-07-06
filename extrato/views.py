150129d0d6a11a356abe241daf9dab75c638e874
Wesley Marcos <wesley.marcos@gec.inatel.br>
2023-07-06 13:26:40 -0300

    extrato: Deixando as categorias e contas dinâmicas

--- extrato/views.py
+++ extrato/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from perfil.models import Categorias, Conta
 
 def novo_valor(request):
     if request.method == "GET":
-        return render(request, 'novo_valor.html')
+        contas = Conta.objects.all()
+        categorias = Categorias.objects.all()
+        return render(request, 'novo_valor.html', {'contas': contas, 'categorias': categorias})
