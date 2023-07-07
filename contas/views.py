96da2fb7f1e74746e5ca1cc235b0029b8d1ed4a6
Wesley Marcos <wesley.marcos@gec.inatel.br>
2023-07-07 10:48:04 -0300

    contas: Definição do else em 'views.py'

--- contas/views.py
+++ contas/views.py
@@ -1,8 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from perfil.models import Categorias
+from .models import ContaPagar
+from django.contrib import messages
+from django.contrib.messages import constants
 
 def definir_contas(request):
 
     if request.method == 'GET':
         categorias = Categorias.objects.all()
         return render(request, 'definir_contas.html', {'categorias': categorias})
+    
+    else:
+        titulo = request.POST.get('titulo')
+        categoria = request.POST.get('categoria')
+        descricao = request.POST.get('descricao')
+        valor = request.POST.get('valor')
+        dia_pagamento = request.POST.get('dia_pagamento')
+
+        conta = ContaPagar(
+            titulo=titulo,
+            categoria_id=categoria,
+            descricao=descricao,
+            valor=valor,
+            dia_pagamento=dia_pagamento
+        )
+
+        conta.save()
+
+        messages.add_message(request, constants.SUCCESS, 'Conta cadastrada com sucesso')
+        return redirect('/contas/definir_contas')
