40ec1565fd8e48c4e344b540165047efd7bbd8b8
Wesley Marcos <wesley.marcos@gec.inatel.br>
2023-07-07 12:18:12 -0300

    perfil: aula até 01:18:00 de 01:47:00

--- perfil/views.py
+++ perfil/views.py
@@ -4,6 +4,7 @@ from .models import Conta, Categorias
 from django.contrib import messages
 from django.contrib.messages import constants
 from .utils import calcula_total
+from extrato.models import Valores
 
 def home(request):
     contas = Conta.objects.all()
@@ -70,4 +71,17 @@ def update_categoria(request, id):
     return redirect('/perfil/gerenciar/')
 
 def dashboard(request):
-    return render(request, 'dashboard.html')
+    dados = {}
+    categorias = Categorias.objects.all()
+
+    for categoria in categorias:
+        total = 0
+        valores = Valores.objects.filter(categoria=categoria)
+
+        for v in valores:
+            total += v.valor
+        
+        dados[categoria.categoria] = total
+
+
+    return render(request, 'dashboard.html', {'labels': list(dados.keys()), 'values': list(dados.values())})
